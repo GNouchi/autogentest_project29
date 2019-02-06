2e00cc65852dc322808e98c3735defd8049eee90
GNouchi <gnouchi7@gmail.com>
2019-02-06 11:30:50 -0800

    .

--- autogentest.js
+++ autogentest.js
@@ -7,25 +7,26 @@ for (let x = 50; x > 0; x--) {
 
 function runtest() {
 // test using search box
-var  testlimit = document.getElementById('testlimit').value;
+var  lowerlimit = document.getElementById('lowerlimit').value;
+var  upperlimit = document.getElementById('upperlimit').value;
 
-// static test
-var testlimit = 870
-var lowerlimit = 600
-// start by logging the limit 
-    if (testlimit > 0 ){
-        console.log(`testlimit is ${testlimit}`);
+// start by logging the limits 
+    if (lowerlimit > 0 || upperlimit > 0  ){
+        console.log(`upperlimit is ${upperlimit} & lowerlimit is ${lowerlimit}`);
     }
     else{
-        console.log(`please enter an upper limit for auto generation`);        
+// static test
+    let lowerlimit = 600;
+    let upperlimit = 800;
+    console.log(`please enter an upper limit for auto generation`);        
     }
 
 // reset testzone
     document.getElementById("testzone").innerHTML =""
 
 // log down to 0
-    for (let i = testlimit;  i > lowerlimit ; i--){
-        console.log(`run #${testlimit - i + 1} testing rand= ${i}` );
+    for (let i = upperlimit;  i > lowerlimit ; i--){
+        console.log(`run #${upperlimit - i + 1} testing rand= ${i}` );
         document.getElementById("testzone").innerHTML += 
         `<a href='https://classic.wowhead.com/item=7423/' 
         rel='rand=${i}'>Testing ${i}</a> <br>`    
